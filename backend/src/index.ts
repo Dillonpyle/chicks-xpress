7d2e925bed84bdf9770c369e9132064c581b985e
Dillonpyle <dillonpyle1991@gmail.com>
2024-12-29 20:56:43 -0600

    set up backend

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import  cors  from 'cors';
+import "dotenv/config"
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", async (req: Request, res: Response) => {
+    res.json({message: "ITS OVER 9000!!!"});
+});
+
+app.listen(9001, () => {
+    console.log("Server is over 9000!!!");
+})
