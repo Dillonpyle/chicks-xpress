da18db71629c55c896f0e6bb2c07150df40fc62a
Dillonpyle <dillonpyle1991@gmail.com>
2025-01-02 20:24:54 -0600

    added update user api

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import User from '../models/user';
 
 const createCurrentUser: express.RequestHandler = async (req: Request, res: Response) => {
@@ -20,6 +20,30 @@ const createCurrentUser: express.RequestHandler = async (req: Request, res: Resp
     }
 };
 
-export default {
-    createCurrentUser,
+const updateCurrentUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const { name, addressLine1, country, city } = req.body;
+        const user = await User.findById(req.userId);
+
+        if(!user){
+            res.status(404).json({ message: 'User not found' });
+            return;
+        }
+
+        user.name = name;
+        user.addressLine1 = addressLine1;
+        user.city = city;
+        user.country = country;
+
+        await user.save();
+        res.status(200).json(user.toObject());
+        
+    } catch (error) {
+        next(error)
+        res.status(500).json({ message: 'Error updating user' });
+    }
 };
+
+
+
+export default {createCurrentUser, updateCurrentUser};
